18521d31f97e638ef950c8de8ccb0feb6f09e23d
Geoffrey Woollard <geoffwoollard@gmail.com>
2019-03-11 16:02:06 -0400

    refactored code and example of how to use in notebook. next step colabnotebook

---
+++ deep_consensus.py
@@ -0,0 +1,95 @@
+import keras
+from keras.layers import Dense, Activation, AveragePooling2D, Dropout, Conv2D, MaxPooling2D, Flatten
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import SGD #RMSprop, Adadelta, Adam, 
+
+def deep_consensus_wrapper(input_shape, # 128,128,1
+	n_classes=2,
+	loss='categorical_crossentropy',
+	metrics=None,
+	conv2d1_k=15,
+	conv2d1_n=8,
+	conv2d2_k=15,
+	conv2d2_n=8,
+	mp3_k=7,
+	mp3_strides=2,
+	conv2d4_k=7,
+	conv2d4_n=8,
+	conv2d5_k=7,
+	conv2d5_n=16,
+	mp6_k=5,
+	mp6_strides=2,
+	conv2d7_k=3,
+	conv2d7_n=32,
+	conv2d8_k=3,
+	conv2d8_n=32,
+	mp9_k=3,
+	mp9_strides=2,
+	conv2d10_k=3,
+	conv2d10_n=64,
+	conv2d11_k=3,
+	conv2d11_n=64,
+	ap12_k=4,
+	ap12_strides=2,
+	dense13_n=512,
+	dropout13_rate=0.5,
+
+	):
+
+	if metrics is None: metrics = ['categorical_accuracy']
+
+	model = Sequential()
+	#2
+	# 1808 = (15*15+1)*8
+	# plus one for bias
+	model.add(Conv2D(conv2d1_n, kernel_size=(conv2d1_k,conv2d1_k), activation='relu',input_shape=input_shape, padding='same'))
+	#3
+	model.add(Conv2D(conv2d2_n, kernel_size=(conv2d2_k,conv2d2_k),padding='same'))
+	# see https://stackoverflow.com/questions/34716454/where-do-i-call-the-batchnormalization-function-in-keras
+	# not sure if batch normaliztion + relu or relu + batch normaliztion
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+
+	#4
+	model.add(MaxPooling2D((mp3_k,mp3_k), strides=mp3_strides,padding='same'))
+	#5 
+	model.add(Conv2D(conv2d4_n, kernel_size=(conv2d4_k,conv2d4_k), activation='relu',padding='same'))
+	#6
+	model.add(Conv2D(conv2d5_n, kernel_size=(conv2d5_k,conv2d5_k),padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+
+	#7
+	model.add(MaxPooling2D((mp6_k,mp6_k), strides=mp6_strides,padding='same'))
+	#8 
+	model.add(Conv2D(conv2d7_n, kernel_size=(conv2d7_k,conv2d7_k), activation='relu',padding='same'))
+	#9
+	model.add(Conv2D(conv2d8_n, kernel_size=(conv2d8_k,conv2d8_k),padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+
+	#10
+	model.add(MaxPooling2D((mp9_k,mp9_k), strides=mp9_strides,padding='same'))
+	#11
+	model.add(Conv2D(conv2d10_n, kernel_size=(conv2d10_k,conv2d10_k), activation='relu',padding='same'))
+	#12
+	model.add(Conv2D(conv2d11_n, kernel_size=(conv2d11_k,conv2d11_k),padding='same'))
+	model.add(Activation('relu'))
+	model.add(BatchNormalization())
+
+	#13
+	model.add(AveragePooling2D(pool_size=(ap12_k, ap12_k), strides=ap12_strides,padding='same'))
+	###
+	model.add(Flatten())
+	model.add(Dense(dense13_n, activation='relu')) # 2097664 = 512*(8*8*64) ; AveragePooling2D size is 8*8*64=4096
+	model.add(Dropout(rate=dropout13_rate)) # note large dropout rate
+
+	model.add(Dense(n_classes, activation='softmax'))
+
+	#model.summary()
+
+	model.compile(optimizer=SGD(), loss=loss, metrics=metrics)
+
+	return(model)
+
---
+++ fit_generator_helper.py
@@ -0,0 +1,86 @@
+#import keras
+#import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+#from sklearn.model_selection import train_test_split
+#from keras.layers import Dense, Activation, AveragePooling2D, Dropout, Conv2D, MaxPooling2D, Flatten
+#from keras.models import Sequential
+#from keras.layers.normalization import BatchNormalization
+#from keras.optimizers import RMSprop, Adadelta, Adam, SGD
+from keras.utils.np_utils import to_categorical 
+
+import mrc
+
+def parse_file(fname,label):
+    #fname = '/Users/gw/Documents/education/2018w/ece1512/project/P11/J85/simulated_particles.mrcs'
+    #label = 0
+    header = mrc.read_header(fname)
+    df = pd.DataFrame({'fname':fname,'idx':range(header['nz']),'class':label})
+    return(df)
+
+def parse_files(fname_list,label_list):
+    df_list = []
+    for fname,label in zip(fname_list,label_list):
+        df_list.append(parse_file(fname,label))
+    
+    return(pd.concat(df_list,axis=0))
+
+def read_particle_from_stack(fname,idx,nx,ny):
+    particles = mrc.read_imgs(fname,idx=idx,num=1).reshape(1,nx,ny)
+    return(particles)
+
+def crop(x,n_crop,nx,ny):
+    x = x[:,int(nx/2-n_crop/2):int(nx/2+n_crop/2),int(nx/2-n_crop/2):int(nx/2+n_crop/2),:]
+    return(x)
+
+def read_particles(dict_list,nx,ny):
+    
+    particle_n=0
+    particles=np.zeros((len(dict_list),nx,ny))
+    for row in dict_list:
+
+        particle = read_particle_from_stack(row['fname'],row['idx'],nx,ny)
+        particles[particle_n,:,:] = particle
+        particle_n+=1
+    return(particles)   
+
+def XY_from_df_batch(df_batch,nx,ny,crop_n=None):
+    dict_list = df_batch.to_dict('records')
+    x_train = read_particles(dict_list,nx,ny)
+    #x_train = crop(x_train,128)
+    X = x_train[:,:,:,np.newaxis]
+    if crop is not None: X = crop(X,crop_n,nx,ny) # match 128x128 in Deep Consensus
+    Y = to_categorical(df_batch['class'].values, num_classes=2,dtype='int') #https://stackoverflow.com/questions/29831489/convert-array-of-indices-to-1-hot-encoded-numpy-array
+    return(X,Y)
+
+
+def image_loader(df,batch_size,**kwargs):
+    
+    L = df.shape[0]
+    while True: #this line is just to make the generator infinite, keras needs that
+        
+        batch_start = 0
+        batch_end = batch_size
+        
+        while batch_start < L:
+            limit = min(batch_end,L)
+            X, Y = XY_from_df_batch(df.iloc[batch_start:limit],**kwargs)
+            yield (X,Y) #a tuple with two numpy arrays with batch_size samples 
+            batch_start += batch_size   
+            batch_end += batch_size
+
+
+
+
+
+def main():
+	fname_list = ['/Users/gw/Documents/education/2018w/ece1512/project/P11/J75/simulated_particles.mrcs',
+              '/Users/gw/Documents/education/2018w/ece1512/project/P11/J103/simulated_particles.mrcs']
+	label_list = [0,1]
+	df = parse_files(fname_list,label_list)
+	df = df.sample(df.shape[0])
+	header = mrc.read_header(fname_list[0])
+	nx,ny=header['nx'],header['nx']
+	val_n = 2000
+	df = df.iloc[:-val_n]
+	df_val = df.iloc[-val_n:]
